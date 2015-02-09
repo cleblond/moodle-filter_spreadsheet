be3ff0d74d45ad408009fbc9e51ff1e313bb9dc7
Carl LeBlond <carl.leblond@iup.edu>
2015-02-08 23:43:20 -0500

    fixed issue with chart and spreadsheet on same page

--- filter.php
+++ filter.php
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * Serve question type files
  *
@@ -23,95 +22,84 @@
  * @copyright  2014 onwards Carl LeBlond 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-
 defined('MOODLE_INTERNAL') || die();
-
 class filter_spreadsheet extends moodle_text_filter {
-  
-
     public function filter($text, array $options = array()) {
         global $CFG, $USER;
-        //$str = '<table>test</table><table class="spreadsheet someOtherClass">content</table>';
-  /*      $search = '/<div.*?class="eo_spreadsheet".*?sheet="(.*?)".*?math="(.*?)".*?group="(.*?)".*?readonly="(.*?)".*?uid="(.*?)".*?>(.*?)<\/div>/';   */
-  $search = '/<div.*?class="eo_spreadsheet".*?sheet="(.*?)".*?<\/div>/';
+        $search = '/<div.*?class="eo_spreadsheet (.*?)".*?<\/div>/';
         //$numofmatches = preg_match_all($search, $text, $matches);
-        $id = 1;
-
+        $id     = 1;
         $newtext = preg_replace_callback($search, function($matches) use (&$id) {
             global $CFG, $USER, $DB, $PAGE;
             require_once($CFG->dirroot . "/filter/spreadsheet/codebase/php/grid_cell_connector.php");
-            $key='';
-        //    print_object($matches);
-        //Must be in database already!
-	if($result = $DB->get_record('filter_spreadsheet_sheet', array('sheetid'=>$matches[1]))){;
-          //  echo "sheetuserid=".$result->userid; 
-            if ($result->userid == $USER->id) { 
-            //if($matches[5] == $USER->id or $matches[3] == true){
-                $result = $DB->get_record('filter_spreadsheet_sheet', array('sheetid'=>$matches[1]));
-                //print_object($result);
-                $dbuserid = $result->userid;
-                if ($USER->id == $result->userid) {
-                    $key = $result->accesskey;
-                } else if ($result->groupmode == 1){
-                    $context = $PAGE->context;
-                    $coursecontext = $context->get_course_context();
-                    $courseid = $coursecontext->instanceid; 
-                    $groups = groups_get_user_groups($courseid, $USER->id);
-                    $groupsowner = groups_get_user_groups($courseid, $result->userid);
-                    $intersect = array_intersect($groups[0], $groupsowner[0]);
-                    if(!empty($intersect)){
-                        $key = $result->accesskey; echo "intersect";}
+            $key = '';
+            //Must be in database already!
+            if ($result = $DB->get_record('filter_spreadsheet_sheet', array(
+                'sheetid' => $matches[1]
+            ))) {
+                ;
+                //  echo "sheetuserid=".$result->userid; 
+                if ($result->userid == $USER->id) {
+                    //if($matches[5] == $USER->id or $matches[3] == true){
+                    $result   = $DB->get_record('filter_spreadsheet_sheet', array(
+                        'sheetid' => $matches[1]
+                    ));
+                    //print_object($result);
+                    $dbuserid = $result->userid;
+                    if ($USER->id == $result->userid) {
+                        $key = $result->accesskey;
+                    } else if ($result->groupmode == 1) {
+                        $context       = $PAGE->context;
+                        $coursecontext = $context->get_course_context();
+                        $courseid      = $coursecontext->instanceid;
+                        $groups        = groups_get_user_groups($courseid, $USER->id);
+                        $groupsowner   = groups_get_user_groups($courseid, $result->userid);
+                        $intersect     = array_intersect($groups[0], $groupsowner[0]);
+                        if (!empty($intersect)) {
+                            $key = $result->accesskey;
+                            echo "intersect";
+                        }
                     } else {
                         $key = '';
                     }
+                }
+                $indb = true;
+            } else { //Not in database..give warning and get out!
+                $indb   = false;
+                $script = get_string('sheetnotindb', 'filter_spreadsheet');
+                return $script;
             }
-            $indb = true;
-	} else {   //Not in database..give warning and get out!
-            $indb = false;
-            $script = get_string('sheetnotindb', 'filter_spreadsheet');
-            return $script;
-	}
-        //    echo "key=$key";
-
-            //echo $key;
             $unique = uniqid();
             $script = '<script>
-		func'.$id.' = function() {
-			var dhx_sh1'.$id.' = new dhtmlxSpreadSheet({
-				load: "'.$CFG->wwwroot.'/filter/spreadsheet/codebase/php/data.php",
-				save: "'.$CFG->wwwroot.'/filter/spreadsheet/codebase/php/data.php",
-				parent: "gridobj'.$id.'",
-				icons_path: "'.$CFG->wwwroot.'/filter/spreadsheet/codebase/imgs/icons/",
-				math: true,
-				autowidth: false,
-				autoheight: false
-			}); 
-			dhx_sh1'.$id.'.load("'.$matches[1].'" , "'.$key.'");
-		};
-	        </script>
-                <div class="ssheet_cont" id="gridobj'.$id.'"></div>';
+        func' . $id . ' = function() {
+            var dhx_sh1' . $id . ' = new dhtmlxSpreadSheet({
+                load: "' . $CFG->wwwroot . '/filter/spreadsheet/codebase/php/data.php",
+                save: "' . $CFG->wwwroot . '/filter/spreadsheet/codebase/php/data.php",
+                parent: "gridobj' . $id . '",
+                icons_path: "' . $CFG->wwwroot . '/filter/spreadsheet/codebase/imgs/icons/",
+                math: true,
+                autowidth: false,
+                autoheight: false
+            }); 
+            dhx_sh1' . $id . '.load("' . $matches[1] . '" , "' . $key . '");
+        };
+            </script>
+                <div class="ssheet_cont" id="gridobj' . $id . '"></div>';
             $id++;
             return $script;
-
         }, $text, -1, $count);
-
-    if ($count !== 0) {
-    $onload = '<script type="text/javascript">window.onload = function() {';
-    for ($i=1; $i<$id; $i++) {
-        $onload .= 'func'.$i.'();';
-    } 
-    $onload .= '}</script>';
-
-
-    $script = '<script src="'.$CFG->wwwroot.'/filter/spreadsheet/codebase/spreadsheet.php?load=js"></script>';
-            $script .= '<link rel="stylesheet" href="'.$CFG->wwwroot.'/filter/spreadsheet/codebase/dhtmlx_core.css">
-                       <link rel="stylesheet" href="'.$CFG->wwwroot.'/filter/spreadsheet/codebase/dhtmlxspreadsheet.css">
-                       <link rel="stylesheet" href="'.$CFG->wwwroot.'/filter/spreadsheet/codebase/dhtmlxgrid_wp.css">';
-
-    $newtext = $script.$onload.$newtext;
-    }
-    return $newtext;
+        if ($count !== 0) {
+            $onload = '<script type="text/javascript">window.onload = function() {';
+            for ($i = 1; $i < $id; $i++) {
+                $onload .= 'func' . $i . '();';
+            }
+            $onload .= '}</script>';
+            $script = '<script src="' . $CFG->wwwroot . '/filter/spreadsheet/codebase/spreadsheet.php?load=js"></script>';
+            $script .= '<link rel="stylesheet" href="' . $CFG->wwwroot . '/filter/spreadsheet/codebase/dhtmlx_core.css">
+                       <link rel="stylesheet" href="' . $CFG->wwwroot . '/filter/spreadsheet/codebase/dhtmlxspreadsheet.css">
+                       <link rel="stylesheet" href="' . $CFG->wwwroot . '/filter/spreadsheet/codebase/dhtmlxgrid_wp.css">';
+            $newtext = $script . $onload . $newtext;
+        }
+        return $newtext;
     }
 }
-
